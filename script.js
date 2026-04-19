1335e9763322d7ac700d7ebbc19b3f6bb4b22630
Manthan <manthanpanchal810@gmail.com>
2026-04-19 17:12:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
+var userName = prompt("Enter your name:");
+alert("Welcome " + userName);
+
 function addTask() {
+    console.log("Button clicked");
+
     var input = document.getElementById("taskInput");
     var task = input.value;
 
     if (task === "") {
-        alert("Enter something");
+        alert("Please enter a task");
+        return;
+    }
+
+    var check = confirm("Do you want to add this task?");
+    if (check === false) {
+        console.log("User cancelled");
         return;
     }
 
@@ -11,10 +22,17 @@ function addTask() {
     li.innerHTML = task;
 
     li.onclick = function () {
-        li.style.textDecoration = "line-through";
+        alert("Task clicked");
+
+        var done = confirm("Mark as completed?");
+        if (done === true) {
+            li.style.textDecoration = "line-through";
+        }
     };
 
     document.getElementById("taskList").appendChild(li);
+
+    console.log("Task added: " + task);
 
     input.value = "";
 }
